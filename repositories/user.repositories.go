87de55906e0ef4300b3312e938c07be136b17464
wallacemachado <wallacefm18@gmail.com>
2021-08-23 20:07:07 -0300

    add repositories

---
+++ repositories/user.repositories.go
@@ -0,0 +1,25 @@
+package repositories
+
+import (
+	"golang-mongo/models"
+)
+
+func Create(user models.User) error {
+
+	return nil
+}
+
+func Read() models.Users {
+
+	return nil
+}
+
+func Update() error {
+
+	return nil
+}
+
+func Delete() error {
+
+	return nil
+}
